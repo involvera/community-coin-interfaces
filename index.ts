047e697422a65203e3925fef0babbe9f2eb9f5f1
fantasim <siuol@gmx.com>
2022-03-28 10:47:26 +0300

    ...

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Buffer } from 'buffer'
 export type TByte = 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|102|103|104|105|106|107|108|109|110|111|112|113|114|115|116|117|118|119|120|121|122|123|124|125|126|127|128|129|130|131|132|133|134|135|136|137|138|139|140|141|142|143|144|145|146|147|148|149|150|151|152|153|154|155|156|157|158|159|160|161|162|163|164|165|166|167|168|169|170|171|172|173|174|175|176|177|178|179|180|181|182|183|184|185|186|187|188|189|190|191|192|193|194|195|196|197|198|199|200|201|202|203|204|205|206|207|208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241|242|243|244|245|246|247|248|249|250|251|252|253|254|255
+export type T_REWARD = 'upvote' | 'reward0' | 'reward1' | 'reward2'
 
 export interface IUserVote {
     has_approved: boolean
@@ -108,9 +109,9 @@ export interface ICostProposal {
     thread: number
     proposal: number
     upvote: number
-    reaction_0: number
-    reaction_1: number
-    reaction_2: number
+    reward0: number
+    reward1: number
+    reward2: number
 }
 
 export interface IScriptOrigin{
@@ -160,17 +161,17 @@ export interface IWallet {
     constitution: IConstitutionRule
 }
 
-export interface IReactionCount {
+export interface IRewardCount {
     n_upvote: number
-    n_reward_0: number
-    n_reward_1: number
-    n_reward_2: number
+    n_reward0: number
+    n_reward1: number
+    n_reward2: number
 }
 
 export interface IRewardCollectionPut {
     value: number
     recipient: string
-    reaction_count: IReactionCount
+    reward_count: IRewardCount
     last_at: number
 }
 
@@ -257,15 +258,15 @@ export interface ISocietyStats {
 
 export interface IRewardSummary {
     value: number
-    reaction_count: IReactionCount
+    reward_count: IRewardCount
     last_at: number
     thread_pkh: string
 }
 
 export interface IThreadReward {
     thread_pkh: string
-    reaction_count: IReactionCount
-    user_reaction_count: IReactionCount
+    reward_count: IRewardCount
+    user_reward_count: IRewardCount
 }
 
 export interface IHeaderSignature {
