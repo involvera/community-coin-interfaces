6288e29689366e4b1775533c9234d56ed1b28f2e
fantasim <siuol@gmx.com>
2022-08-27 16:19:55 +0300

    1.2.1

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,14 @@
 import typescript from 'rollup-plugin-typescript2'
 import { uglify } from 'rollup-plugin-uglify';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 import pkg from './package.json'
 
 const config = {
     input: './index.ts',
-    external: [ 'wallet-util' ],
     output: [
         {
-            globals: {
-                'wallet-util': 'wallet-util',
-                'buffer': 'buffer'
-            },
             file: pkg.main,
             format: 'umd',
-            name: 'wallet-script'
+            name: 'community-coin-types'
         },
     ],
     plugins: [
@@ -24,16 +16,8 @@ const config = {
             tsconfig: 'tsconfig.json',
             tsconfigOverride: { compilerOptions: { module: 'es2015' } },
         }),
-        nodeResolve({
-            preferBuiltins: false
-        }),
-        nodePolyfills(),
-        commonjs(),
+        uglify()
     ]
-}
-
-if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(uglify());
 }
 
 export default config
